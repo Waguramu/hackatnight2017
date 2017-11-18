1b34a6b97d728ee79010cb97f45a788e51b2c1b1
Wagram Airian <vagram.airiian@tum.de>
2017-11-18 14:49:53 +0100

    Profile page setup

---
+++ routes/profile.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET profile page. */
+router.get('/profile', function(req, res, next) {
+    res.render('profile', {
+        name: "John Doe"
+    });
+});
+
+module.exports = router;
